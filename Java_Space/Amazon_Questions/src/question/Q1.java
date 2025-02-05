5ca964a65cc521863bbdaed6a8d4b56cb14780a3
poornasasank21 <poornasasank@gmail.com>
2025-02-05 17:10:57 +0530

    first question
    d

---
+++ Java_Space/Amazon_Questions/src/question/Q1.java
@@ -0,0 +1,23 @@
+package question;
+
+import java.util.HashMap;
+
+public class Q1
+{
+    public int[] twoSum(int[] nums, int target)
+    {
+        HashMap<Integer,Integer> hm=new HashMap<>();
+        for(int i=0;i<nums.length;i++)
+        {
+            if(hm.containsKey(target-nums[i]))
+            {
+                return new int[]{i,hm.get(target-nums[i])};
+            }
+            else
+            {
+                hm.put(nums[i],i);
+            }
+        }
+        return new int[]{0,0};
+    }
+}
